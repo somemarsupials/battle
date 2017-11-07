1dada33dd357e8bb15c193bf12906c8cd8f4f053
Robert Clayton <robbie.c.clayton@gmail.com>
2017-11-07 14:46:17 +0000

    test infrastructure set up

--- battle.rb
+++ battle.rb
@@ -4,6 +4,9 @@ class Battle < Sinatra::Base
   set :port, 5000
 
   get '/' do
-    'You are at Battle root.'
+    'Testing infrastructure working!'
   end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
